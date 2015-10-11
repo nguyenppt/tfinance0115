1d250545cd72e783acd5e59b3c13c3d4cc590e89
nguyenhien1807 <nguyenhien1807@users.noreply.github.com>
2015-10-11 21:25:58 +0700

    [Hien Nguyen] Commit SQL script for changes in DB

--- DBScripts/alter table BEXPORT_LC.sql
+++ DBScripts/alter table BEXPORT_LC.sql
@@ -1,3 +1,3 @@
 alter table BEXPORT_LC
 --add PaymentFull bit
-add PaymentAmount float null
+add LCType float null
---
+++ DBScripts/alter table BLCTYPES.sql
@@ -0,0 +1,3 @@
+alter table [dbo].[BLCTYPES]
+--add Type column
+add [Type] [nvarchar](10) null
